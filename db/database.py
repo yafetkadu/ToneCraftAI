f08073860cbec8e2883698fd3435c10e2b403e46
Yafet Befkadu <yami@Yafets-MacBook-Pro.local>
2025-09-17 18:13:32 -0400

    setting up files

---
+++ db/database.py
@@ -0,0 +1,21 @@
+# Imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from dotenv import load_dotenv
+import os
+
+#loading .env
+load_dotenv('../.env')
+
+
+#Initizlers
+URL_DATABASE = os.getenv("URL_DATABASE")
+
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
