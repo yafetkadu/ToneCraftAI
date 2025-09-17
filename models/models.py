a21d1c65a8ef090dbaa148466a1394e2278186c3
Yafet Befkadu <yami@Yafets-MacBook-Pro.local>
2025-09-17 18:45:08 -0400

    Routes have been setup

--- models/models.py
+++ models/models.py
@@ -1,19 +1,6 @@
 # Imports
-from sqlalchemy import Boolean, Column, Integer, String
-from db.database import Base
+#from sqlalchemy import Boolean, Column, Integer, String
+#from db.database import Base
 
 
 # Setup Model Functions
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True)
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50))
-    content = Column(String(100))
-    user_id = Column(Integer)
